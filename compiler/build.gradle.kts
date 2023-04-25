31c0ab1d6aeac1f396e4ff4cfb9794d0f7526185
jisungbin <ji@sungb.in>
2023-04-25 11:40:25 +0900

    Remove the annotation module dependency on the compiler module

--- compiler/build.gradle.kts
+++ compiler/build.gradle.kts
@@ -34,7 +34,6 @@ tasks.withType<Test>().configureEach {
 dependencies {
     compileOnly("org.jetbrains.kotlin:kotlin-compiler-embeddable:1.8.20")
     implementation("com.google.auto.service:auto-service-annotations:1.0.1")
-    implementation(project(":annotation"))
     ksp("dev.zacsweers.autoservice:auto-service-ksp:1.0.0")
 
     testImplementation("io.kotest:kotest-runner-junit5:5.6.1")
